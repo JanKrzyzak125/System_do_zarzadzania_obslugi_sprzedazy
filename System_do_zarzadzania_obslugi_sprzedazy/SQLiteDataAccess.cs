e98ad6e24657c314b5e7abbbd08b8f7648e06275
Błażej Bałdyga <125223@student.uni.opole.pl>
2021-03-05 12:35:50 +0100

    Dodano klase firma oraz polaczenie z baza danych ktorej uzyjemy do wczytywania

---
+++ System_do_zarzadzania_obslugi_sprzedazy/SQLiteDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace System_do_zarzadzania_obslugi_sprzedazy
+{
+    class SQLiteDataAccess
+    {
+        public static List<Firma>LoadPeople()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Firma>("select * from Firma", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
