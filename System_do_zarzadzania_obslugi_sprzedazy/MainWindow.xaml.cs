35df66ca7021faaabf0234cb54b17a52517b0293
Mucha125 <jankrzy9999@gmail.com>
2021-03-10 11:03:09 +0100

    Uporządkowanie i przypisanie nazw przycisków oraz otwieranie okienek

--- System_do_zarzadzania_obslugi_sprzedazy/MainWindow.xaml.cs
+++ System_do_zarzadzania_obslugi_sprzedazy/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace System_do_zarzadzania_obslugi_sprzedazy
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private bool invoiceOpen=false;
+
         List<Firma> firma = new List<Firma>();
         public MainWindow()
         {
@@ -68,6 +70,66 @@ namespace System_do_zarzadzania_obslugi_sprzedazy
         //       // WiredUpPeople();
         //        //SQLiteDataAccess.DeletePeople((Firma)FirmaListBox.SelectedItem);
         //    }
+        }
+
+        private void Invoice_Open(object sender, RoutedEventArgs e)
+        {
+            
+            Invoice invoice=new Invoice();
+            //if (invoiceOpen== false) 
+            invoice.Show();
+            //invoiceOpen=true;
+            
+            
+        }
+
+        private void Storage_Open(object sender, RoutedEventArgs e)
+        {
+            Storage storage = new Storage();
+            storage.Show();
+        }
+
+        private void Settlements_Open(object sender, RoutedEventArgs e)
+        {
+            Settlements settlements = new Settlements();
+            settlements.Show();
+        }
+
+        private void Contractors_Open(object sender, RoutedEventArgs e)
+        {
+            Contractors contractors = new Contractors();
+            contractors.Show();
+        }
+
+        private void Statments_Open(object sender, RoutedEventArgs e)
+        {
+            Statments statments = new Statments();
+            statments.Show();
+
+        }
+
+        private void VATRegister_Open(object sender, RoutedEventArgs e)
+        {
+            VATRegister vATRegister = new VATRegister();
+            vATRegister.Show();
+        }
+
+        private void Print_Open(object sender, RoutedEventArgs e)
+        {
+            Print print = new Print();
+            print.Show();
+        }
+
+        private void Search_Open(object sender, RoutedEventArgs e)
+        {
+            Search search = new Search();
+            search.Show();
+        }
+
+        private void ToSquare_Open(object sender, RoutedEventArgs e)
+        {
+            ToSquare toSquare = new ToSquare();
+            toSquare.Show();
         }
     }
 }
