713a143a6de2da16de7b2c2573001ffebb004ec9
Radek Barth <radzio.b@interia.eu>
2021-04-05 15:35:46 +0200

    Dodano auto uzupełnianie nazwy produktu.

--- System_do_zarzadzania_obslugi_sprzedazy/Winows/NewProduct.xaml.cs
+++ System_do_zarzadzania_obslugi_sprzedazy/Winows/NewProduct.xaml.cs
@@ -72,16 +72,19 @@ namespace System_do_zarzadzania_obslugi_sprzedazy.Winows
         {
             int idInvoice = Int32.Parse(InvoiceID.Text);
             int idProduct = Int32.Parse(ProductID.Text);
-            string productName = ProductName.Text;
+            string productName = ProductNameComboBox.Text;
             int quantity = Int32.Parse(ProductQuantity.Text);
             string quantityUnits = ProductQuantityUnit.Text;
             int nettoPrice = Int32.Parse(ProductNettoPrice.Text);
             int bruttoPrice = Int32.Parse(ProductBruttoPrice.Text);
             int vat = Int32.Parse(ProductVat.Text);
+
+            //Wykorzystamy metode indexof w celu wyciagniecia numeru indexu danego elementu z listy unitname jesli index = -1 nalezy dodac nowy element do listy
             InvoiceProduct invoiceProduct = new InvoiceProduct(idInvoice, idProduct, productName, quantity, quantityUnits, nettoPrice, bruttoPrice, vat);
             SQLiteDataAccess.SaveInvoiceProduct(invoiceProduct);
             this.Close();
         }
+
 
         private void ProductNettoPrice_TextChanged(object sender, TextChangedEventArgs e)
         {
